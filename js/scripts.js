e79b0da79fdbc484fe106fb90940a038153a21a9
chris huson and jose rodriguez <jose@>
2017-12-06 10:28:56 -0800

    add question parameter

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,11 @@
 
 var leapYear = function(year) {
-  if (year % 4 === 0) {
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
     return true;
   } else {
     return false;
   }
 };
-
 
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
